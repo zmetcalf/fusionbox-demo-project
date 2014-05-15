7a8f39939c949e41e3e77bf999add80f08b7fce4
Zachery Metcalf <zachery.metcalf@gmail.com>
2014-05-15 13:50:05 -0600

    Removed erronious slideshow from admin

--- slideshow/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from widgy.admin import WidgyAdmin
-
-from slideshow.models import Slideshow, Slide
-
-
-admin.site.register(Slideshow, WidgyAdmin)
-admin.site.register(Slide, WidgyAdmin)
