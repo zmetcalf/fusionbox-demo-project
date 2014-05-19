da6190c246f1c3cc5c34432a165eb193244a907d
Zachery Metcalf <zachery.metcalf@gmail.com>
2014-05-19 11:19:08 -0600

    Future fix to proxy-widget

--- fusionbox_demo_project/models.py
+++ fusionbox_demo_project/models.py
@@ -1,7 +1,5 @@
 import widgy
 
-from django import forms
-
 from widgy.contrib.form_builder.models import FormInput
 
 widgy.unregister(FormInput)
@@ -13,16 +11,5 @@ class FormInput(FormInput):
         proxy = True
 
     @property
-    def widget(self):
-        attrs = {
-            'type': self.type,
-            'class': 'form-control'
-        }
-        if self.required:
-            attrs['required'] = 'required'
-
-        if self.type == 'date':
-            # Use type text because Kalendae doesn't play well with type=date
-            attrs['type'] = 'text'
-            attrs['class'] += ' date auto-kal'
-        return forms.TextInput(attrs=attrs)
+    def widget_input_class(self):
+        return 'form-control'
