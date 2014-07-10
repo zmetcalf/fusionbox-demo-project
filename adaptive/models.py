e13e6116b8075f41d5dfa5e108ca75b16d084ec0
Zachery Metcalf <zachery.metcalf@gmail.com>
2014-07-10 15:05:24 -0600

    Fixed device_info

--- adaptive/models.py
+++ adaptive/models.py
@@ -4,20 +4,17 @@ from widgy.models import Content
 
 from widgy.utils import update_context, render_to_string
 
-from responsive.context_processors import device_info
-
 
 @widgy.register
 class Adaptive(Content):
-    def render(self, context, template=None):
-        template = ''
-        size = device_info(context)
-        if size['device_info']['type'] == 'tablet':
+    def render(self, context):
+        template = 'widgy/adaptive/render.html'
+        size = context.get('device_info')
+
+        if size['type'] == 'tablet':
             template = 'widgy/adaptive/tablet.html'
-        elif size['device_info']['type'] == 'phone':
+        elif size['type'] == 'phone':
             template = 'widgy/adaptive/phone.html'
-        else:
-            template = 'widgy/adaptive/render.html'
 
         with update_context(context, {'self': self}):
             return render_to_string(template, context)
